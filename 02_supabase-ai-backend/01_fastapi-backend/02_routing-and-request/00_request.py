5cdce96034f5faa4b3355482cd8203ed587b0436
jmlee <jmlee@tonesol.com>
2026-07-16 11:15:37 +0900

    0716

--- 02_supabase-ai-backend/01_fastapi-backend/02_routing-and-request/00_request.py
+++ 02_supabase-ai-backend/01_fastapi-backend/02_routing-and-request/00_request.py
@@ -3,19 +3,14 @@ uvicorn 00_request:app --reload
 """
 
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from mymodels import Customer, CustomerDetail
+
 
 app = FastAPI(
     title = "Request Test",
     description = "request test",
     version = "0.1"
 )
-
-class Customer(BaseModel):
-    id:str = Field(min_length=3, examples=["id01"])
-    pwd:str = Field(min_length=4, examples=["pwd01"])
-    name:str = Field(min_length=5, examples=["james"])
-    age:int = Field(ge=1, examples=[20])
 
 
 @app.get("/health")
@@ -28,3 +23,14 @@ def register(customer:Customer):
     print(customer.name)
     print(customer.age)
     return {"msg":f"{customer.name} 가입축하!"}
+
+# path paramter
+# 127.0.0.1:8000/get/id01
+@app.get("/get/{input_id}")
+def get(input_id : str):
+    customer = Customer(
+        id = input_id,
+        name = "james",
+        age = 20
+    )
+    return customer
